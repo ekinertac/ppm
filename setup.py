e001023d16f7026a1d0478ff6b2c9be784f1c11d
ekinertac <ekinertac@gmail.com>
2019-11-19 15:44:26 +0300

    change project name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ppm",  # Replace with your own username
+    name="pip-ppm",  # Replace with your own username
     version="0.0.1",
     author="Ekin Ertaç",
     author_email="ekinertac@gmail.com",
